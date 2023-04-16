35ec980c417b311ab92bf8620c00996e782ac162
dariomrk <dariomrkonjic1@gmail.com>
2023-04-16 15:00:21 +0200

    Add ConfigurationHelper, SnouterDbContext.

---
+++ Common/helpers/ConfigurationHelper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace Common.Helpers
+{
+    public class ConfigurationHelper
+    {
+        /// <summary>
+        /// Gets the configuration from the application.<i>&lt;ASPNETCORE_ENVIRONMENT&gt;</i>.json file.<br/>
+        /// To set the desired environment run the following command in the Package Manager Console:
+        /// <code>
+        /// $env:ASPNETCORE_ENVIRONMENT = "<i>&lt;ASPNETCORE_ENVIRONMENT&gt;</i>"
+        /// </code>
+        /// The available environments are located in the <i>appsettings.json</i> configuration file under the "Environments" property.
+        /// </summary>
+        public static IConfigurationRoot GetConfiguration()
+        {
+            var environments = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build()
+                .GetValue<string>("Environments")
+                ?.Split(";")
+                ?? throw new InvalidOperationException("Configuration value \"Environments\" is missing in appsettings.json.");
+            
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
+                ?? throw new InvalidOperationException($"Environment variable ASPNETCORE_ENVIRONMENT is not set.");
+
+            if (!environments.Contains(environment))
+                throw new ArgumentException("ASPNETCORE_ENVIROMENT is not set to a valid value.");
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.{environment}.json")
+                .Build();
+
+            return configuration;
+        }
+    }
+}
---
+++ Data/SnouterDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class SnouterDbContext : DbContext
+    {
+        public SnouterDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        #region DbSets
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
