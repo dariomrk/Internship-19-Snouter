4eb9ade9ac37d4243e6d50ca94a87aff9f9cedb5
dariomrk <dariomrkonjic1@gmail.com>
2023-04-16 15:33:48 +0200

    Add SnouterDbContextFactory.

---
+++ Data/SnouterDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Common.Helpers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Data
+{
+    public class SnouterDbContextFactory : IDesignTimeDbContextFactory<SnouterDbContext>
+    {
+        public SnouterDbContext CreateDbContext(string[] args)
+        {
+            var connectionString = ConfigurationHelper
+                .GetConfiguration()
+                .GetConnectionString("Database")
+                ?? throw new ArgumentNullException("Database connection string is not specified.");
+
+            var options = new DbContextOptionsBuilder<SnouterDbContext>()
+                .UseNpgsql(connectionString)
+                .Options;
+
+            return new SnouterDbContext(options);
+        }
+    }
+}
