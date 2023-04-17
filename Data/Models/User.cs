07646c7eaa83437592b2d980bef2a7ef045cd8c1
dariomrk <dariomrkonjic1@gmail.com>
2023-04-17 22:00:33 +0200

    Add User model.

---
+++ Data/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Data.Models
+{
+    public class User : BaseEntity<int>
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string UsernameSlug { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+    }
+}
