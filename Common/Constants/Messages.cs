e72a0168ad3ed0f00ff2e5567f5fe140c619470c
dariomrk <dariomrkonjic1@gmail.com>
2023-04-18 19:39:33 +0200

    Remove unused messages.

--- Common/Constants/Messages.cs
+++ Common/Constants/Messages.cs
@@ -2,12 +2,10 @@
 {
     public static class Messages
     {
-        public const string CaughtException = "An exception has been caught";
+        public const string CaughtException = "An exception has been caught.";
         public const string IdInUse = "The specified id is already in use.";
         public const string EntityDoesNotExist = "Entity: {0} with the specified id: {1} does not exist.";
         public const string RepositoryActionFailed = "Repository action failed. Check the repository log for more details.";
-        public const string CreatedInstanceOf = "Created new instance of: ";
-        public const string NoCityRecord = "There is no record of the city: {0}. Add the city first.";
         public const string NoInformationAvailable = "No information available.";
     }
 }
