59bbfe91daeebefef7730cb6474e0f5ff88ed09e
dariomrk <dariomrkonjic1@gmail.com>
2023-04-20 00:36:57 +0200

    Implement ExceptionHandlerMiddleware.

---
+++ Api/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using Contracts.Responses;
+using Serilog;
+using System.Net;
+
+namespace Api.Middleware
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exception)
+            {
+                Log.Error(exception, exception.Message);
+                await HandleExceptionAsync(context, exception);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(
+            HttpContext context,
+            Exception exception,
+            CancellationToken cancellationToken = default)
+        {
+
+            var statusCode = exception switch
+            {
+                ArgumentException => HttpStatusCode.BadRequest,
+                InvalidOperationException => HttpStatusCode.BadRequest,
+                _ => HttpStatusCode.InternalServerError,
+            };
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)statusCode;
+            await context.Response.WriteAsJsonAsync(new ExceptionResponse
+            {
+                StatusCode = statusCode,
+                Message = exception.Message,
+            });
+
+        }
+    }
+}
---
+++ Contracts/Responses/ExceptionResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace Contracts.Responses
+{
+    public class ExceptionResponse
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string Message { get; set; }
+    }
+}
